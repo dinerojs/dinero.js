cd3e76fdd91903fbc8ce4000f3347586f3c064a3
Sarah Dayan <dayan.sarah@gmail.com>
2018-03-01 17:26:51 +0100

    Remove unnecessary doclets

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,8 @@
-/**
- * Default values for Dinero objects
- * @type {Object}
- */
 export const Defaults = {
   defaultAmount: 0,
   defaultCurrency: 'USD'
 }
 
-/**
- * Global values for Dinero objects
- * @type {Object}
- */
 export const Globals = {
   globalLocale: 'en-GB',
   globalDisplay: 'symbol',
