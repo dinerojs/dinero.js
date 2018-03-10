c723ec6c3605951a8f933eb3c71049600b2773ec
Sarah Dayan <dayan.sarah@gmail.com>
2018-03-10 16:07:37 +0100

    Add links for formats

--- src/services/settings.js
+++ src/services/settings.js
@@ -4,8 +4,8 @@
  * You can override default values for all subsequent Dinero objects by changing them directly on the global `Dinero` object.
  * Existing instances won't be affected.
  *
- * @property {Number} defaultAmount - The default amount for new Dinero objects.
- * @property {String} defaultCurrency - The default currency for new Dinero objects.
+ * @property {Number} defaultAmount - The default amount for new Dinero objects (see {@link module:Dinero Dinero} for format).
+ * @property {String} defaultCurrency - The default currency for new Dinero objects (see {@link module:Dinero Dinero} for format).
  *
  * @example
  * // Will set currency to 'EUR' for all Dinero objects.
@@ -24,10 +24,8 @@ export const Defaults = {
  * You can override global values for all subsequent Dinero objects by changing them directly on the global `Dinero` object.
  * Existing instances won't be affected.
  *
- * @property {String}  globalLocale - The global locale for new Dinero objects.
- * @property {String}  globalCurrencyDisplay - The global currency display mode for new Dinero objects.
- * @property {Boolean} globalUseGrouping - The global grouping option for new Dinero objects.
- * @property {Number}  globalMinimumFractionDigits - The global number of fraction digits for new Dinero objects.
+ * @property {String}  globalLocale - The global locale for new Dinero objects (see {@link module:Dinero~setLocale setLocale} for format).
+ * @property {String}  globalFormat - The global format for new Dinero objects (see {@link module:Dinero~toFormat toFormat} for format).
  *
  * @example
  * // Will set locale to 'fr-FR' for all Dinero objects.
