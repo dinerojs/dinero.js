e440f01972c3077423a48fdda3c274c98e3c3658
Sarah Dayan <dayan.sarah@gmail.com>
2018-03-29 23:25:43 +0200

    build(test): Migrate tests to Jest

--- test/unit/calculator.spec.js
+++ test/unit/calculator.spec.js
@@ -1,28 +1,26 @@
-import chai from 'chai'
 import Calculator from '../../src/services/calculator'
 
-const expect = chai.expect
 const calculator = Calculator()
 
 describe('Calculator', () => {
   describe('#add()', () => {
-    it('should return the sum of two numbers', () => {
-      expect(calculator.add(10, 20)).to.equal(30)
+    test('should return the sum of two numbers', () => {
+      expect(calculator.add(10, 20)).toBe(30)
     })
   })
   describe('#subtract()', () => {
-    it('should return the difference of two numbers', () => {
-      expect(calculator.subtract(20, 10)).to.equal(10)
+    test('should return the difference of two numbers', () => {
+      expect(calculator.subtract(20, 10)).toBe(10)
     })
   })
   describe('#multiply()', () => {
-    it('should return the product of two numbers', () => {
-      expect(calculator.multiply(10, 20)).to.equal(200)
+    test('should return the product of two numbers', () => {
+      expect(calculator.multiply(10, 20)).toBe(200)
     })
   })
   describe('#divide()', () => {
-    it('should return the quotient of two numbers', () => {
-      expect(calculator.divide(20, 2)).to.equal(10)
+    test('should return the quotient of two numbers', () => {
+      expect(calculator.divide(20, 2)).toBe(10)
     })
   })
 })
--- test/unit/helpers.spec.js
+++ test/unit/helpers.spec.js
@@ -1,24 +1,21 @@
-import chai from 'chai'
 import * as Helpers from '../../src/services/helpers'
-
-const expect = chai.expect
 
 describe('Helpers()', () => {
   describe('#isNumeric()', () => {
-    it('should return true with an integer', () => {
-      expect(Helpers.isNumeric(5)).to.be.true
+    test('should return true with an integer', () => {
+      expect(Helpers.isNumeric(5)).toBe(true)
     })
-    it('should return true with a float', () => {
-      expect(Helpers.isNumeric(5.5)).to.be.true
+    test('should return true with a float', () => {
+      expect(Helpers.isNumeric(5.5)).toBe(true)
     })
-    it('should return true with a string', () => {
-      expect(Helpers.isNumeric('5')).to.be.true
+    test('should return true with a string', () => {
+      expect(Helpers.isNumeric('5')).toBe(true)
     })
-    it('should return false with NaN', () => {
-      expect(Helpers.isNumeric(NaN)).to.be.false
+    test('should return false with NaN', () => {
+      expect(Helpers.isNumeric(NaN)).toBe(false)
     })
-    it('should return false with Infinity', () => {
-      expect(Helpers.isNumeric(Infinity)).to.be.false
+    test('should return false with Infinity', () => {
+      expect(Helpers.isNumeric(Infinity)).toBe(false)
     })
   })
 })
