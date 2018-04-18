b0a9e25eebefbc39c90c46f8ed6aa8058ddf04ba
Sarah Dayan <dayan.sarah@gmail.com>
2018-04-18 19:04:31 +0200

    refactor(helpers): Add more helpers

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -9,3 +9,27 @@
 export function isNumeric(value) {
   return !isNaN(parseInt(value)) && isFinite(value)
 }
+
+/**
+ * Returns whether a value is a percentage.
+ * @ignore
+ *
+ * @param  {}  percentage - The percentage to test.
+ *
+ * @return {Boolean}
+ */
+export function isPercentage(percentage) {
+  return isNumeric(percentage) && percentage <= 100 && percentage >= 0
+}
+
+/**
+ * Returns whether an array of ratios is valid.
+ * @ignore
+ *
+ * @param  {}  ratios - The ratios to test.
+ *
+ * @return {Boolean}
+ */
+export function areValidRatios(ratios) {
+  return ratios.length > 0 && ratios.every(ratio => ratio > 0)
+}
--- test/unit/helpers.spec.js
+++ test/unit/helpers.spec.js
@@ -18,4 +18,29 @@ describe('Helpers()', () => {
       expect(Helpers.isNumeric(Infinity)).toBe(false)
     })
   })
+  describe('#isPercentage()', () => {
+    test('should return false with a negative number', () => {
+      expect(Helpers.isPercentage(-5)).toBe(false)
+    })
+    test('should return false with a number above 100', () => {
+      expect(Helpers.isPercentage(101)).toBe(false)
+    })
+    test('should return true with a number between 0 and 100 (included)', () => {
+      expect(Helpers.isPercentage(50)).toBe(true)
+    })
+  })
+  describe('#areValidRatios()', () => {
+    test('should return true with percentage style', () => {
+      expect(Helpers.areValidRatios([50, 50])).toBe(true)
+    })
+    test('should return true with ratio style', () => {
+      expect(Helpers.areValidRatios([1, 3])).toBe(true)
+    })
+    test('should return false with one ratio to 0', () => {
+      expect(Helpers.areValidRatios([1, 0])).toBe(false)
+    })
+    test('should return false with an empty array', () => {
+      expect(Helpers.areValidRatios([])).toBe(false)
+    })
+  })
 })
