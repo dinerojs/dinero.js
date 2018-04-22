466d6b55101f613b50023b9c061f2c26a4b53104
Sarah Dayan <dayan.sarah@gmail.com>
2018-04-23 00:11:14 +0200

    test(helpers#isFloat): Add property-based tests

--- test/property/helpers.spec.js
+++ test/property/helpers.spec.js
@@ -23,6 +23,14 @@ describe('Helpers', () => {
       jsc.assert(jsc.forall(jsc.uint8, a => !Helpers.isEven(a * 2 - 1)))
     })
   })
+  describe('#isFloat', () => {
+    test('should return true for floats', () => {
+      jsc.assert(jsc.forall(jsc.uint8, a => Helpers.isFloat(a * 2 - 1 / 2)))
+    })
+    test('should return false for integers', () => {
+      jsc.assert(jsc.forall(jsc.uint8, a => !Helpers.isFloat(a * 2 / 2)))
+    })
+  })
   describe('#isHalf', () => {
     test('should return true for half numbers', () => {
       jsc.assert(jsc.forall(jsc.uint8, a => Helpers.isHalf((a * 2 - 1) / 2)))
