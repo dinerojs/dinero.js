9175a33ec8d18ce3d3bb901f229ede463cf845f5
Sarah Dayan <dayan.sarah@gmail.com>
2018-04-22 23:52:42 +0200

    test(helpers#isPercentage): Add property-based tests

--- test/property/helpers.spec.js
+++ test/property/helpers.spec.js
@@ -2,6 +2,19 @@ import * as Helpers from '../../src/services/helpers'
 import jsc from 'jsverify'
 
 describe('Helpers', () => {
+  describe('#isPercentage', () => {
+    test('should return true for numbers between 0 and 100', () => {
+      jsc.assert(jsc.forall(jsc.nat(100), a => Helpers.isPercentage(a)))
+    })
+    test('should return false for numbers above 100', () => {
+      jsc.assert(
+        jsc.forall(jsc.uint8, a => !Helpers.isPercentage(a + (100 - a + 1)))
+      )
+    })
+    test('should return false for numbers below 0', () => {
+      jsc.assert(jsc.forall(jsc.uint8, a => !Helpers.isPercentage(-a - 1)))
+    })
+  })
   describe('#isEven', () => {
     test('should return true for even numbers', () => {
       jsc.assert(jsc.forall(jsc.uint8, a => Helpers.isEven(a * 2)))
