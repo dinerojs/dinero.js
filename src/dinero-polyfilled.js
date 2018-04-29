d716a9ff77780c8b9c2053e64939728fb77c75e2
Sarah Dayan <dayan.sarah@gmail.com>
2018-04-29 04:13:22 +0200

    fix(dinero-polyfilled): Remove promise polyfill (not yet needed)

--- src/dinero-polyfilled.js
+++ src/dinero-polyfilled.js
@@ -5,7 +5,6 @@ import 'core-js/fn/array/keys'
 import 'core-js/fn/object/assign'
 import 'core-js/fn/number/is-integer'
 import 'core-js/fn/math/sign'
-import 'core-js/fn/promise'
 
 import Dinero from './dinero'
 export default Dinero
