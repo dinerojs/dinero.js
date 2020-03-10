85be9ef9a8911810de76e457be57edbbf3114fef
Sarah Dayan <dayan.sarah@gmail.com>
2020-03-10 20:48:02 +0100

    ci: upgrade Travis workflow (#114)

--- tasks/after_success.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-  npm run docs
-  npm run build
-  npm run travis-deploy-once "npm run semantic-release"
-  curl -Is https://purge.jsdelivr.net/npm/dinero.js/build/umd/dinero.min.js
-fi
