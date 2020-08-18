c6f2dd18b5cb9872cff6a49dd01f3740b2651884
Sarah Dayan <dayan.sarah@gmail.com>
2020-08-18 16:43:02 +0200

    style: lint

--- packages/core/src/formatter/types/Formatter.ts
+++ packages/core/src/formatter/types/Formatter.ts
@@ -1,7 +1,5 @@
 import { PureDinero } from '@dinero.js/pure';
 
-type Formatter<TAmount> = (
-  pureDinero: PureDinero<TAmount>
-) => string;
+type Formatter<TAmount> = (pureDinero: PureDinero<TAmount>) => string;
 
 export default Formatter;
