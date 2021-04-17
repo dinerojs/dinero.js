75cbeaf2146aff5b33bb760fbc83842f19063048
Sarah Dayan <dayan.sarah@gmail.com>
2021-04-17 20:10:45 +0200

    chore: re-export userland types

--- packages/dinero.js/index.ts
+++ packages/dinero.js/index.ts
@@ -1,1 +1,4 @@
+export { Dinero, DineroFactory, DineroOptions, DineroSnapshot, Formatter, Rates, RoundingOptions, Transformer } from '@dinero.js/core';
+export { Currency } from '@dinero.js/currencies';
+
 export * from './src';
