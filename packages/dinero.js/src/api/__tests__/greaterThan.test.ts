9bb26f8902e934f5c446415d06161c240bdc8150
Sarah Dayan <dayan.sarah@gmail.com>
2021-04-18 16:05:55 +0200

    test: add missing tests for greaterThan function

--- packages/dinero.js/src/api/__tests__/greaterThan.test.ts
+++ packages/dinero.js/src/api/__tests__/greaterThan.test.ts
@@ -1,23 +1,74 @@
-import { USD } from '@dinero.js/currencies';
-import { dinero, greaterThan } from '../../..';
+import { USD, EUR } from '@dinero.js/currencies';
+import { dinero, greaterThan, unsafeGreaterThan } from '../../..';
 
 describe('greaterThan', () => {
-  it('returns false when the first amount is less than the other', () => {
-    const d1 = dinero({ amount: 500, currency: USD });
-    const d2 = dinero({ amount: 800, currency: USD });
+  describe('safe', () => {
+    it('returns false when the first amount is less than the other', () => {
+      const d1 = dinero({ amount: 500, currency: USD });
+      const d2 = dinero({ amount: 800, currency: USD });
 
-    expect(greaterThan(d1, d2)).toBe(false);
+      expect(greaterThan(d1, d2)).toBe(false);
+    });
+    it('returns false when amounts are equal', () => {
+      const d1 = dinero({ amount: 500, currency: USD });
+      const d2 = dinero({ amount: 500, currency: USD });
+
+      expect(greaterThan(d1, d2)).toBe(false);
+    });
+    it('returns true when the first amount is greater than the other', () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 500, currency: USD });
+
+      expect(greaterThan(d1, d2)).toBe(true);
+    });
+    it('normalizes the result to the highest scale', () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 5000, currency: USD, scale: 3 });
+
+      expect(greaterThan(d1, d2)).toBe(true);
+    });
+    it('throws when using different currencies', () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 500, currency: EUR });
+
+      expect(() => {
+        greaterThan(d1, d2);
+      }).toThrowErrorMatchingInlineSnapshot(
+        `"Dinero objects don't have the same currency."`
+      );
+    });
   });
-  it('returns false when amounts are equal', () => {
-    const d1 = dinero({ amount: 500, currency: USD });
-    const d2 = dinero({ amount: 500, currency: USD });
+  describe('unsafe', () => {
+    it('returns false when the first amount is less than the other', () => {
+      const d1 = dinero({ amount: 500, currency: USD });
+      const d2 = dinero({ amount: 800, currency: USD });
 
-    expect(greaterThan(d1, d2)).toBe(false);
-  });
-  it('returns true when the first amount is greater than the other', () => {
-    const d1 = dinero({ amount: 800, currency: USD });
-    const d2 = dinero({ amount: 500, currency: USD });
+      expect(unsafeGreaterThan(d1, d2)).toBe(false);
+    });
+    it('returns false when amounts are equal', () => {
+      const d1 = dinero({ amount: 500, currency: USD });
+      const d2 = dinero({ amount: 500, currency: USD });
 
-    expect(greaterThan(d1, d2)).toBe(true);
+      expect(unsafeGreaterThan(d1, d2)).toBe(false);
+    });
+    it('returns true when the first amount is greater than the other', () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 500, currency: USD });
+
+      expect(unsafeGreaterThan(d1, d2)).toBe(true);
+    });
+    it("doesn't normalize the result to the highest scale, resulting in an incorrect comparison", () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 5000, currency: USD, scale: 3 });
+
+      expect(unsafeGreaterThan(d1, d2)).toBe(false);
+    });
+    it("doesn't throw when using different currencies, resulting in an incorrect comparison", () => {
+      const d1 = dinero({ amount: 800, currency: USD });
+      const d2 = dinero({ amount: 500, currency: EUR });
+
+      expect(() => unsafeGreaterThan(d1, d2)).not.toThrow();
+      expect(unsafeGreaterThan(d1, d2)).toBe(true);
+    });
   });
 });
