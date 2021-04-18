a687899d3a2619aca48d667fb22021696761cd9d
Sarah Dayan <dayan.sarah@gmail.com>
2021-04-18 15:51:15 +0200

    test: add missing tests for equal function

--- packages/dinero.js/src/api/__tests__/equal.test.ts
+++ packages/dinero.js/src/api/__tests__/equal.test.ts
@@ -26,4 +26,16 @@ describe('equal', () => {
 
     expect(equal(d1, d2)).toBe(false);
   });
+  it('returns true when amounts are equal after normalization', () => {
+    const d1 = dinero({ amount: 500, currency: USD });
+    const d2 = dinero({ amount: 5000, currency: USD, scale: 3 });
+
+    expect(equal(d1, d2)).toBe(true);
+  });
+  it('returns false when amounts are not equal after normalization', () => {
+    const d1 = dinero({ amount: 500, currency: USD });
+    const d2 = dinero({ amount: 500, currency: USD, scale: 3 });
+
+    expect(equal(d1, d2)).toBe(false);
+  });
 });
