a3fbbce95cdedb6c80bb28612f8b5090048afcaa
Sarah Dayan <dayan.sarah@gmail.com>
2021-05-19 00:20:04 +0200

    build: configure Ship.js

---
+++ ship.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-commonjs, functional/no-expression-statement, functional/immutable-data */
+module.exports = {
+  monorepo: {
+    mainVersionFile: 'lerna.json',
+    // We rely on Lerna to bump our dependencies.
+    packagesToBump: [],
+    packagesToPublish: [
+      'packages/calculator-number',
+      'packages/core',
+      'packages/currencies',
+      'packages/dinero.js',
+    ],
+  },
+  publishCommand({ tag }) {
+    return `yarn publish --access public --tag ${tag}`;
+  },
+  versionUpdated({ exec, version }) {
+    // Update package dependencies
+    exec(
+      `yarn lerna version ${version} --exact --no-git-tag-version --no-push --yes`
+    );
+
+    // Ship.js reads JSON and writes with `fs.writeFileSync(JSON.stringify(json, null, 2))`
+    // which causes a lint error in the `lerna.json` file.
+    exec('yarn eslint lerna.json --fix');
+  },
+  // Skip preparation if it contains only `chore` commits
+  shouldPrepare({ releaseType, commitNumbersPerType }) {
+    const { fix = 0 } = commitNumbersPerType;
+
+    return releaseType !== 'patch' || fix !== 0;
+  },
+};
