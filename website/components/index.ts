ee0494d63a5935daa295f563489b98e5e4cfcb18
Sarah Dayan <dayan.sarah@gmail.com>
2021-06-08 21:00:19 +0200

    docs: export MDXComponents last so it picks up imports from thr same file

--- website/components/index.ts
+++ website/components/index.ts
@@ -2,7 +2,7 @@ export * from './Alert';
 export * from './Image';
 export * from './Link';
 export * from './Logo';
-export * from './MDXComponents';
 export * from './Panel';
 export * from './Parameters';
 export * from './Signature';
+export * from './MDXComponents';
