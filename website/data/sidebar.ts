c45427335faac22a5d4d4a2d25ffa036b7c9a91b
Sarah Dayan <dayan.sarah@gmail.com>
2021-06-10 01:13:28 +0200

    docs: turn sidebar into a tree

--- website/data/sidebar.ts
+++ website/data/sidebar.ts
@@ -1,3 +1,62 @@
+import { Sitemap, Resource } from '../utils/sitemap';
+
+export const tree = Sitemap.create();
+
+tree.add(Resource.create({ label: 'Getting started', path: '/getting-started' }));
+tree.add(Resource.create({ label: 'Quick start', path: '/getting-started/quick-start' }));
+tree.add(Resource.create({ label: 'Upgrade guide', path: '/getting-started/upgrade-guide' }));
+tree.add(Resource.create({ label: 'Optimizing for production', path: '/getting-started/optimizing-for-production' }));
+tree.add(Resource.create({ label: 'Compatibility', path: '/getting-started/compatibility' }));
+
+tree.add(Resource.create({ label: 'Core concepts', path: '/core-concepts' }));
+tree.add(Resource.create({ label: 'Amount', path: '/core-concepts/amount' }));
+tree.add(Resource.create({ label: 'Currency', path: '/core-concepts/currency' }));
+tree.add(Resource.create({ label: 'Scale', path: '/core-concepts/scale' }));
+tree.add(Resource.create({ label: 'Mutations', path: '/core-concepts/mutations' }));
+tree.add(Resource.create({ label: 'Comparisons', path: '/core-concepts/comparisons' }));
+tree.add(Resource.create({ label: 'Formatting', path: '/core-concepts/formatting' }));
+
+tree.add(Resource.create({ label: 'Advanced', path: '/advanced' }));
+tree.add(Resource.create({ label: 'Using different amount types', path: '/advanced/using-different-amount-types' }));
+tree.add(Resource.create({ label: 'Transporting and restoring', path: '/advanced/transporting-and-restoring' }));
+tree.add(Resource.create({ label: 'Formatting non-decimal currencies', path: '/advanced/formatting-non-decimal-currencies' }));
+
+tree.add(Resource.create({ label: 'API Reference', path: '/api' }));
+tree.add(Resource.create({ label: 'Mutations', path: '/api/mutations' }));
+tree.add(Resource.create({ label: 'Add', path: '/api/mutations/add' }));
+tree.add(Resource.create({ label: 'Subtract', path: '/api/mutations/subtract' }));
+tree.add(Resource.create({ label: 'Multiply', path: '/api/mutations/multiply' }));
+tree.add(Resource.create({ label: 'Allocate', path: '/api/mutations/allocate' }));
+tree.add(Resource.create({ label: 'Conversions', path: '/api/conversions' }));
+tree.add(Resource.create({ label: 'Convert', path: '/api/conversions/convert' }));
+tree.add(Resource.create({ label: 'Normalize scale', path: '/api/conversions/normalize-scale' }));
+tree.add(Resource.create({ label: 'Transform scale', path: '/api/conversions/transform-scale' }));
+tree.add(Resource.create({ label: 'Trim scale', path: '/api/conversions/trim-scale' }));
+tree.add(Resource.create({ label: 'Comparisons', path: '/api/comparisons' }));
+tree.add(Resource.create({ label: 'Equal', path: '/api/comparisons/equal' }));
+tree.add(Resource.create({ label: 'Greater than', path: '/api/comparisons/greater-than' }));
+tree.add(Resource.create({ label: 'Greater than or equal', path: '/api/comparisons/greater-than-or-equal' }));
+tree.add(Resource.create({ label: 'Less than', path: '/api/comparisons/less-than' }));
+tree.add(Resource.create({ label: 'Less than or equal', path: '/api/comparisons/less-than-or-equal' }));
+tree.add(Resource.create({ label: 'Minimum', path: '/api/comparisons/minimum' }));
+tree.add(Resource.create({ label: 'Maximum', path: '/api/comparisons/maximum' }));
+tree.add(Resource.create({ label: 'Is zero', path: '/api/comparisons/is-zero' }));
+tree.add(Resource.create({ label: 'Is positive', path: '/api/comparisons/is-positive' }));
+tree.add(Resource.create({ label: 'Is negative', path: '/api/comparisons/is-negative' }));
+tree.add(Resource.create({ label: 'Have same amount', path: '/api/comparisons/have-same-amount' }));
+tree.add(Resource.create({ label: 'Have same currency', path: '/api/comparisons/have-same-currency' }));
+tree.add(Resource.create({ label: 'Has sub-units', path: '/api/comparisons/has-sub-units' }));
+tree.add(Resource.create({ label: 'Formatting', path: '/api/formatting' }));
+tree.add(Resource.create({ label: 'To format', path: '/api/formatting/to-format' }));
+tree.add(Resource.create({ label: 'To snapshot', path: '/api/formatting/to-snapshot' }));
+tree.add(Resource.create({ label: 'To unit', path: '/api/formatting/to-unit' }));
+tree.add(Resource.create({ label: 'To rounded unit', path: '/api/formatting/to-rounded-unit' }));
+
+tree.add(Resource.create({ label: 'FAQ', path: '/faq' }));
+tree.add(Resource.create({ label: 'Does Dinero.js support cryptocurrencies?', path: '/faq/does-dinerojs-support-cryptocurrencies' }));
+tree.add(Resource.create({ label: 'How do I calculate a percentage?', path: '/faq/how-do-i-calculate-a-percentage' }));
+tree.add(Resource.create({ label: 'How can I create Dinero objects from floats?', path: '/faq/how-can-i-create-dinero-objects-from-floats' }));
+
 export const sidebar = {
   children: [
     {
