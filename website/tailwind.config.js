331ccba7df909fa61b11c153eb911017ebbb4a99
Sarah Dayan <dayan.sarah@gmail.com>
2021-06-10 23:46:16 +0200

    chore: set up JIT

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +13,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
