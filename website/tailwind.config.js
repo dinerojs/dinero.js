cfc67ab866485af56dd4b349a8637070936627b8
Sarah Dayan <dayan.sarah@gmail.com>
2021-06-26 11:32:08 +0200

    feat: customize shadows

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -28,6 +28,18 @@ module.exports = {
         '"Noto Color Emoji"',
       ],
     },
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(17, 17, 26, 0.05)',
+      DEFAULT: '0px 1px 0px rgba(17, 17, 26, 0.05), 0px 0px 8px rgba(17, 17, 26, 0.1)',
+      md: '0px 0px 16px rgba(17, 17, 26, 0.1)',
+      lg: '0px 4px 16px rgba(17, 17, 26, 0.05), 0px 8px 32px rgba(17, 17, 26, 0.05)',
+      xl: '0px 4px 16px rgba(17, 17, 26, 0.1), 0px 8px 32px rgba(17, 17, 26, 0.05)',
+      '2xl': '0px 8px 24px rgba(17, 17, 26, 0.1), 0px 16px 48px rgba(17, 17, 26, 0.1)',
+      '3xl': '0px 4px 16px rgba(17, 17, 26, 0.1), 0px 8px 24px rgba(17, 17, 26, 0.1), 0px 16px 56px rgba(17, 17, 26, 0.1)',
+      '4xl': '0px 8px 24px rgba(17, 17, 26, 0.1), 0px 16px 56px rgba(17, 17, 26, 0.1), 0px 24px 80px rgba(17, 17, 26, 0.1)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+    },
     extend: {
       colors: {
         gray: blueGray,
