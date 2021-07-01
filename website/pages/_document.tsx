5bc444a89f4b741d63f14690724bca6b70ece851
Sarah Dayan <dayan.sarah@gmail.com>
2021-07-01 23:57:58 +0200

    feat: add favicon

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -15,7 +15,14 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en" className="spt-24">
-        <Head />
+        <Head>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className="sm:overflow-auto">
           <Main />
           <NextScript />
