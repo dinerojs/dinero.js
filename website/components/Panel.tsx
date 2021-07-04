126f1075cf8a8ed4232f507664689ee03d1fef3e
Sarah Dayan <dayan.sarah@gmail.com>
2021-07-04 19:23:57 +0200

    docs: udpate component

--- website/components/Panel.tsx
+++ website/components/Panel.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
-
 import { ArrowRightIcon } from './icons';
+import { ExternalLink, InternalLink } from './Link';
 
 export type PanelProps = {
   groups: {
@@ -23,18 +22,17 @@ export function Panel({ groups }: PanelProps) {
                 {links.map(({ title, url }) => {
                   const isExternalLink = !url.startsWith('/') && !url.startsWith('#');
                   const { host } = isExternalLink ? new URL(url) : { host: undefined };
+                  const LinkComponent = isExternalLink ? ExternalLink : InternalLink;
 
                   return (
                     <div key={title} className="py-3 border-t border-gray-100 first:border-0">
-                      <Link href={url}>
-                        <a className="flex items-start w-full space-x-3 transition-opacity duration-100 ease-in-out group opacity-90 hover:opacity-100">
-                          <span className="flex items-baseline flex-1 space-x-1">
-                            <span className="group-hover:underline">{title}</span>
-                            {host && <span className="text-xs text-gray-400">({host})</span>}
-                          </span>
-                          <ArrowRightIcon className="h-4 mt-0.5 text-gray-300 text-opacity-0 transition duration-150 ease-in-out transform -translate-x-2 group-hover:translate-x-0 group-hover:text-opacity-100" />
-                        </a>
-                      </Link>
+                      <LinkComponent href={url} className="flex items-start w-full space-x-3 transition-opacity duration-100 ease-in-out group opacity-90 hover:opacity-100">
+                        <span className="flex-1">
+                          <span className="group-hover:underline">{title}</span>
+                          {host && <span className="ml-1 text-xs text-gray-400">({host})</span>}
+                        </span>
+                        <ArrowRightIcon className="h-4 mt-0.5 text-gray-300 text-opacity-0 transition duration-150 ease-in-out transform -translate-x-2 group-hover:translate-x-0 group-hover:text-opacity-100" />
+                      </LinkComponent>
                     </div>
                   )
                 })}
