e37a770f16277f6d20d5d2884dba2a25f7683f9d
Sarah Dayan <dayan.sarah@gmail.com>
2021-07-09 22:46:20 +0200

    docs: test out ignored deployments

---
+++ website/scripts/ignore-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
