b35a1c85060ff4ebc4faa3281aa73790969fa510
Sarah Dayan <dayan.sarah@gmail.com>
2021-07-14 13:04:59 +0200

    docs: add starter sandbox

---
+++ examples/starter/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable functional/no-expression-statement, no-console */
+import { USD } from '@dinero.js/currencies';
+import { dinero, toFormat, toSnapshot } from 'dinero.js';
+
+const transformer = (props) => `${props.currency.code} ${props.amount}`;
+
+const d = dinero({ amount: 999, currency: USD });
+
+console.log('Snapshot:', toSnapshot(d));
+console.log('Formatted:', toFormat(d, transformer));
