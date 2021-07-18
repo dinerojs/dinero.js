fe1b9e9591d7718bc957c8936ba15d68ddf5fce1
Sarah Dayan <dayan.sarah@gmail.com>
2021-07-18 16:29:00 +0200

    docs: fix scroll padding

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -14,7 +14,7 @@ export default class CustomDocument extends Document {
 
   render() {
     return (
-      <Html lang="en" className="spt-24">
+      <Html lang="en" className="spt-32">
         <Head>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
