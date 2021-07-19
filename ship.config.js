273546b40db2f653a5f3dbe21e7764b166797b19
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-07-19 19:11:29 +0200

    build: format lerna.json on release

--- ship.config.js
+++ ship.config.js
@@ -20,6 +20,10 @@ module.exports = {
     exec(
       `yarn lerna version ${version} --exact --no-git-tag-version --no-push --yes`
     );
+
+    // Ship.js reads JSON and writes with `fs.writeFileSync(JSON.stringify(json, null, 2))`
+    // which causes a formatting error in the `lerna.json` file.
+    exec('yarn prettier lerna.json --write');
   },
   // Skip preparation if it contains only `chore` commits
   shouldPrepare({ releaseType, commitNumbersPerType }) {
