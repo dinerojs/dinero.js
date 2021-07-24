538cf81a1fc5ea3421f2892fed7db31475a2f287
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-07-24 13:54:35 +0200

    chore: bootstrap pricing example in React

---
+++ examples/pricing-react/tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-commonjs, functional/immutable-data, functional/no-expression-statement */
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html', './src/**/*.{js,jsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
