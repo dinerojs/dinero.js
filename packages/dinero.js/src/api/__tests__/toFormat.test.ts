857568e655dad0ae626a3409a7075ffc49839145
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-08-02 01:39:39 +0200

    docs: update toFormat references

--- packages/dinero.js/src/api/__tests__/toFormat.test.ts
+++ packages/dinero.js/src/api/__tests__/toFormat.test.ts
@@ -1,245 +1,114 @@
+import { calculator } from '@dinero.js/calculator-bigint';
 import { MGA, USD } from '@dinero.js/currencies';
-import Big from 'big.js';
 
-import { toFormat } from '..';
-import {
-  castToBigintCurrency,
-  castToBigjsCurrency,
-  createNumberDinero,
-  createBigintDinero,
-  createBigjsDinero,
-} from '../../../../../test/utils';
+import { toFormat, dinero, createDinero } from '../../..';
 
 describe('toFormat', () => {
-  describe('number', () => {
-    const dinero = createNumberDinero;
+  describe('decimal currencies', () => {
+    it('formats the Dinero object using the `decimal` value', () => {
+      const d = dinero({ amount: 1050, currency: USD });
 
-    describe('decimal currencies', () => {
-      it('formats the Dinero object using the `decimal` value', () => {
-        const d = dinero({ amount: 1050, currency: USD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 10.50');
-      });
-      it('formats the Dinero object and pads the decimal part', () => {
-        const d = dinero({ amount: 500, currency: USD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 5.00');
-      });
-      it('formats the Dinero object using the scale', () => {
-        const d = dinero({ amount: 4545, currency: USD, scale: 3 });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 4.545');
-      });
-      it('formats using the Intl API after casting `decimal` into a number', () => {
-        const d = dinero({ amount: 1050, currency: USD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => {
-            return Number(decimal).toLocaleString('en-US', {
-              style: 'currency',
-              currency: currency.code,
-            });
-          })
-        ).toBe('$10.50');
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: 500, currency: USD });
-
-        expect(
-          toFormat(d, ({ units }) => `${units[0]} dollars, ${units[1]} cents`)
-        ).toBe('5 dollars, 0 cents');
-      });
+      expect(
+        toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
+      ).toBe('USD 10.50');
     });
-    describe('non-decimal currencies', () => {
-      it('does not have access to a `decimal` value', () => {
-        const d = dinero({ amount: 13, currency: MGA });
+    it('formats the Dinero object and pads the decimal part', () => {
+      const d = dinero({ amount: 500, currency: USD });
 
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('does not consider decimal a currency which compiles to a multiple of 10', () => {
-        const d = dinero({
-          amount: 13,
-          currency: { code: 'ABC', exponent: 1, base: [5, 2] },
-        });
+      expect(
+        toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
+      ).toBe('USD 5.00');
+    });
+    it('formats the Dinero object using the scale', () => {
+      const d = dinero({ amount: 4545, currency: USD, scale: 3 });
 
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: 13, currency: MGA });
+      expect(
+        toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
+      ).toBe('USD 4.545');
+    });
+    it('formats using the Intl API after casting `decimal` into a number', () => {
+      const d = dinero({ amount: 1050, currency: USD });
 
-        expect(
-          toFormat(
-            d,
-            ({ units }) => `${units[0]} ariary, ${units[1]} iraimbilanja`
-          )
-        ).toBe('2 ariary, 3 iraimbilanja');
+      expect(
+        toFormat(d, ({ decimal, currency }) => {
+          return Number(decimal).toLocaleString('en-US', {
+            style: 'currency',
+            currency: currency.code,
+          });
+        })
+      ).toBe('$10.50');
+    });
+    it('formats the Dinero object using the `units` value', () => {
+      const d = dinero({ amount: 500, currency: USD });
+
+      expect(
+        toFormat(d, ({ units }) => `${units[0]} dollars, ${units[1]} cents`)
+      ).toBe('5 dollars, 0 cents');
+    });
+  });
+  describe('non-decimal currencies', () => {
+    it('does not have access to a `decimal` value', () => {
+      const d = dinero({ amount: 13, currency: MGA });
+
+      expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
+    });
+    it('does not consider decimal a currency which compiles to a multiple of 10', () => {
+      const d = dinero({
+        amount: 13,
+        currency: { code: 'ABC', exponent: 1, base: [5, 2] },
       });
+
+      expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
+    });
+    it('formats a single-base Dinero object using the `units` value', () => {
+      const d = dinero({ amount: 13, currency: MGA });
+
+      expect(
+        toFormat(
+          d,
+          ({ units }) => `${units[0]} ariary, ${units[1]} iraimbilanja`
+        )
+      ).toBe('2 ariary, 3 iraimbilanja');
+    });
+    it('formats a multi-base Dinero object using the `units` value', () => {
+      function transformer({ units }) {
+        const amounts = [
+          { amount: units[0], label: '🍩' },
+          { amount: units[1], label: '🍪' },
+          { amount: units[2], label: '🍭' },
+        ];
+
+        return amounts
+          .filter(({ amount }) => amount > 0)
+          .map(({ amount, label }) => `${amount} ${label}`)
+          .join(' and ');
+      }
+
+      const d = dinero({
+        amount: 720,
+        currency: {
+          code: 'POP',
+          base: [30, 16],
+          exponent: 1,
+        },
+      });
+
+      expect(toFormat(d, transformer)).toBe('1 🍩 and 15 🍪');
     });
   });
   describe('bigint', () => {
-    const dinero = createBigintDinero;
-    const bigintUSD = castToBigintCurrency(USD);
-    const bigintMGA = castToBigintCurrency(MGA);
+    const dineroBigint = createDinero({ calculator });
+    const BigUSD = { code: 'USD', base: 10n, exponent: 2n };
 
-    describe('decimal currencies', () => {
-      it('formats the Dinero object using the `decimal` value', () => {
-        const d = dinero({ amount: 1050n, currency: bigintUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 10.50');
+    it('formats the Dinero object using the `decimal` value', () => {
+      const d = dineroBigint({
+        amount: 1000000000000000050n,
+        currency: BigUSD,
       });
-      it('formats the Dinero object using the `decimal` value with large integers', () => {
-        const d = dinero({ amount: 1000000000000000050n, currency: bigintUSD });
 
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 10000000000000000.50');
-      });
-      it('formats the Dinero object and pads the decimal part', () => {
-        const d = dinero({ amount: 500n, currency: bigintUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 5.00');
-      });
-      it('formats the Dinero object using the scale', () => {
-        const d = dinero({ amount: 4545n, currency: bigintUSD, scale: 3n });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 4.545');
-      });
-      it('formats using the Intl API after casting `decimal` into a number', () => {
-        const d = dinero({ amount: 1050n, currency: bigintUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => {
-            return Number(decimal).toLocaleString('en-US', {
-              style: 'currency',
-              currency: currency.code,
-            });
-          })
-        ).toBe('$10.50');
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: 500n, currency: bigintUSD });
-
-        expect(
-          toFormat(d, ({ units }) => `${units[0]} dollars, ${units[1]} cents`)
-        ).toBe('5 dollars, 0 cents');
-      });
-    });
-    describe('non-decimal currencies', () => {
-      it('does not have access to a `decimal` value', () => {
-        const d = dinero({ amount: 13n, currency: bigintMGA });
-
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('does not consider decimal a currency which compiles to a multiple of 10', () => {
-        const d = dinero({
-          amount: 13n,
-          currency: { code: 'ABC', exponent: 1n, base: [5n, 2n] },
-        });
-
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: 13n, currency: bigintMGA });
-
-        expect(
-          toFormat(
-            d,
-            ({ units }) => `${units[0]} ariary, ${units[1]} iraimbilanja`
-          )
-        ).toBe('2 ariary, 3 iraimbilanja');
-      });
-    });
-  });
-  describe('Big.js', () => {
-    const dinero = createBigjsDinero;
-    const bigjsUSD = castToBigjsCurrency(USD);
-    const bigjsMGA = castToBigjsCurrency(MGA);
-
-    describe('decimal currencies', () => {
-      it('formats the Dinero object using the `decimal` value', () => {
-        const d = dinero({ amount: new Big(1050), currency: bigjsUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 10.50');
-      });
-      it('formats the Dinero object using the `decimal` value with large integers', () => {
-        const d = dinero({ amount: new Big('1000000000000000050'), currency: bigjsUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 10000000000000000.50');
-      });
-      it('formats the Dinero object and pads the decimal part', () => {
-        const d = dinero({ amount: new Big(500), currency: bigjsUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 5.00');
-      });
-      it('formats the Dinero object using the scale', () => {
-        const d = dinero({ amount: new Big(4545), currency: bigjsUSD, scale: new Big(3) });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
-        ).toBe('USD 4.545');
-      });
-      it('formats using the Intl API after casting `decimal` into a number', () => {
-        const d = dinero({ amount: new Big(1050), currency: bigjsUSD });
-
-        expect(
-          toFormat(d, ({ decimal, currency }) => {
-            return Number(decimal).toLocaleString('en-US', {
-              style: 'currency',
-              currency: currency.code,
-            });
-          })
-        ).toBe('$10.50');
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: new Big(500), currency: bigjsUSD });
-
-        expect(
-          toFormat(d, ({ units }) => `${units[0]} dollars, ${units[1]} cents`)
-        ).toBe('5 dollars, 0 cents');
-      });
-    });
-    describe('non-decimal currencies', () => {
-      it('does not have access to a `decimal` value', () => {
-        const d = dinero({ amount: new Big(13), currency: bigjsMGA });
-
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('does not consider decimal a currency which compiles to a multiple of 10', () => {
-        const d = dinero({
-          amount: new Big(13),
-          currency: { code: 'ABC', exponent: new Big(1), base: [new Big(5), new Big(2)] },
-        });
-
-        expect(toFormat(d, ({ decimal }) => decimal)).toBeUndefined();
-      });
-      it('formats the Dinero object using the `units` value', () => {
-        const d = dinero({ amount: new Big(13), currency: bigjsMGA });
-
-        expect(
-          toFormat(
-            d,
-            ({ units }) => `${units[0]} ariary, ${units[1]} iraimbilanja`
-          )
-        ).toBe('2 ariary, 3 iraimbilanja');
-      });
+      expect(
+        toFormat(d, ({ decimal, currency }) => `${currency.code} ${decimal}`)
+      ).toBe('USD 10000000000000000.50');
     });
   });
 });
