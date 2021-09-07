4bcc9265c4c989a982a344575ce8b553804d3772
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-09-07 22:46:42 +0200

    style: lint

--- packages/core/src/helpers/__tests__/assert.test.ts
+++ packages/core/src/helpers/__tests__/assert.test.ts
@@ -4,9 +4,13 @@ const errorMessage = 'Some error message.';
 
 describe('assert', () => {
   it("doesn't throw when the condition is met", () => {
-    expect(() => assert(true, errorMessage)).not.toThrow();
+    expect(() => assert(true, errorMessage)).not.toThrow(
+      new Error('[Dinero.js] Some error message.')
+    );
   });
   it("throws when the condition isn't met", () => {
-    expect(() => assert(false, errorMessage)).toThrow();
+    expect(() => assert(false, errorMessage)).toThrow(
+      new Error('[Dinero.js] Some error message.')
+    );
   });
 });
