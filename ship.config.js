b028bfb5f2a3be51f784af474d601a1a799ac0d2
John Hooks <bitmachina@outlook.com>
2022-11-06 11:34:19 -0800

    build: use turbo over lerna

--- ship.config.js
+++ ship.config.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-commonjs, functional/no-expression-statement, functional/immutable-data */
 module.exports = {
   monorepo: {
-    mainVersionFile: 'lerna.json',
-    // We rely on Lerna to bump our dependencies.
+    mainVersionFile: 'package.json',
     packagesToBump: [],
     packagesToPublish: [
       'packages/calculator-bigint',
@@ -14,16 +13,6 @@ module.exports = {
   },
   publishCommand({ tag }) {
     return `yarn publish --access public --tag ${tag}`;
-  },
-  versionUpdated({ exec, version }) {
-    // Update package dependencies
-    exec(
-      `yarn lerna version ${version} --exact --no-git-tag-version --no-push --yes`
-    );
-
-    // Ship.js reads JSON and writes with `fs.writeFileSync(JSON.stringify(json, null, 2))`
-    // which causes a formatting error in the `lerna.json` file.
-    exec('yarn prettier lerna.json --write');
   },
   // Skip preparation if it contains only `chore` commits
   shouldPrepare({ releaseType, commitNumbersPerType }) {
