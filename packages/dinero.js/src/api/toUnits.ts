9662b645025f2b979fc9fdf5454fc6b1801f2f0c
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-11-28 10:07:45 +0100

    fix: use overloads

--- packages/dinero.js/src/api/toUnits.ts
+++ packages/dinero.js/src/api/toUnits.ts
@@ -1,5 +1,14 @@
 import { toUnits as coreToUnits } from '@dinero.js/core';
-import type { ToUnitsParams } from '@dinero.js/core';
+import type { ToUnitsParams, Dinero, Transformer } from '@dinero.js/core';
+
+export function toUnits<TAmount>(
+  dineroObject: Dinero<TAmount>
+): readonly TAmount[];
+
+export function toUnits<TAmount, TOutput>(
+  dineroObject: Dinero<TAmount>,
+  transformer: Transformer<TAmount, TOutput, readonly TAmount[]>
+): TOutput;
 
 /**
  * Get the amount of a Dinero object in units.
