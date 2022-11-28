64facb5cd6b66ebddc0622558a88b3a2e69c6dc7
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-11-28 10:07:45 +0100

    fix: use overloads

--- packages/core/src/api/toSnapshot.ts
+++ packages/core/src/api/toSnapshot.ts
@@ -1,15 +1,21 @@
 import type { Dinero, DineroSnapshot, Transformer } from '../types';
 
-export type ToSnapshotParams<TAmount, TOutput> = readonly [
-  dineroObject: Dinero<TAmount>,
-  transformer?: Transformer<TAmount, TOutput, DineroSnapshot<TAmount>>
-];
+export function toSnapshot<TAmount>(
+  dineroObject: Dinero<TAmount>
+): DineroSnapshot<TAmount>;
 
 export function toSnapshot<TAmount, TOutput>(
-  ...[
-    dineroObject,
-    transformer = ({ value }) => value as TOutput,
-  ]: ToSnapshotParams<TAmount, TOutput>
+  dineroObject: Dinero<TAmount>,
+  transformer: Transformer<TAmount, TOutput, DineroSnapshot<TAmount>>
+): TOutput;
+
+export function toSnapshot<TAmount>(
+  dineroObject: Dinero<TAmount>,
+  transformer: Transformer<
+    TAmount,
+    DineroSnapshot<TAmount>,
+    DineroSnapshot<TAmount>
+  > = ({ value }) => value
 ) {
   const value = dineroObject.toJSON();
 
--- packages/dinero.js/src/api/toDecimal.ts
+++ packages/dinero.js/src/api/toDecimal.ts
@@ -1,5 +1,12 @@
 import { toDecimal as coreToDecimal } from '@dinero.js/core';
-import type { ToDecimalParams } from '@dinero.js/core';
+import type { ToDecimalParams, Dinero, Transformer } from '@dinero.js/core';
+
+export function toDecimal<TAmount>(dineroObject: Dinero<TAmount>): string;
+
+export function toDecimal<TAmount, TOutput>(
+  dineroObject: Dinero<TAmount>,
+  transformer: Transformer<TAmount, TOutput, string>
+): TOutput;
 
 /**
  * Get the amount of a Dinero object in decimal form.
