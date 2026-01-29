ca8f5e2a3ecbd6b17bda5890d6a64070ad664650
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2026-01-29 10:22:10 +0100

    fix(release): use --ignore-optional for yarn install

--- ship.config.js
+++ ship.config.js
@@ -5,6 +5,9 @@ module.exports = {
     packagesToBump: ['packages/*', 'website'],
     packagesToPublish: ['packages/*'],
   },
+  installCommand() {
+    return 'true'; // Skip install, use existing node_modules
+  },
   publishCommand({ tag }) {
     return `yarn publish --access public --tag ${tag} --registry https://registry.npmjs.org`;
   },
